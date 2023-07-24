96c7395da3c7a938065aab10fc503e0204a834a8
zheleznyak <mobobzor@gmail.com>
2023-07-24 21:30:36 +0200

    Displaying Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import useGenres from "../hooks/useGenres";
+import {HStack, Image, List, ListItem, Text} from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/img-url";
 
 const GenreList = () => {
   const { data: genres } = useGenres();
 
   return (
-    <ul>
-      {genres.map(genre => <li key={genre.id}>{ genre.name }</li>)}
-    </ul>
+    <List>
+      {genres.map(genre => (
+        <ListItem key={genre.id} paddingY='5px'>
+          <HStack>
+            <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
+            <Text fontSize='lg'>{genre.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
   );
 };
 
