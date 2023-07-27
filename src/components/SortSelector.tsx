297eb230a0ec430bfd85df000f911e37dae1ee62
zheleznyak <mobobzor@gmail.com>
2023-07-27 10:03:19 +0200

    Sort the games

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -2,23 +2,39 @@ import React from 'react';
 import {Button, Menu, MenuButton, MenuItem, MenuList} from "@chakra-ui/react";
 import {BsChevronDown} from "react-icons/all";
 
-const SortSelector = () => {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrder: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
+  const sortOrders = [
+    { value: '', label: 'Relevance' },
+    { value: '-added', label: 'Date added' },
+    { value: 'name', label: 'Name' },
+    { value: '-released', label: 'Release date' },
+    { value: '-metacritic', label: 'Popularity' },
+    { value: '-rating', label: 'Average rating' },
+  ];
+
+  const currentSortOrder = sortOrders.find(order => order.value === sortOrder);
+
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
         {/*{selectedPlatform?.name || 'Platforms'}*/}
-        Sort by:
+        Sort by: {currentSortOrder?.label || 'Relevance'}
       </MenuButton>
       <MenuList>
-        {/*{data.map(platform => (*/}
-        {/*  <MenuItem*/}
-        {/*    key={platform.id}*/}
-        {/*    onClick={() => onSelectPlatform(platform)}*/}
-        {/*  >*/}
-        {/*    { platform.name }*/}
-        {/*  </MenuItem>*/}
-        {/*))}*/}
-        <MenuItem>TEST</MenuItem>
+        {sortOrders.map(order => (
+          <MenuItem
+            key={order.value}
+            value={order.value}
+            onClick={() => onSelectSortOrder(order.value)}
+          >
+            { order.label }
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -18,7 +18,11 @@ export interface Game {
 
 const useGames = (gameQuery: GameQuery) => useData<Game>(
   '/games',
-  { params: { genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id }},
+  { params: {
+    genres: gameQuery.genre?.id,
+    platforms: gameQuery.platform?.id,
+    ordering: gameQuery.sortOrder
+  }},
   [gameQuery]
 );
 
