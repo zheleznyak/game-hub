06dbcb7b90d31fd38364e372a495edea67939d5c
zheleznyak <mobobzor@gmail.com>
2023-08-25 12:36:00 +0200

    Build game attributes

--- src/entities/game.ts
+++ src/entities/game.ts
@@ -1,9 +1,13 @@
 import {Platform} from "./platform";
+import {Genre} from "./genre";
+import {Publisher} from "./publisher";
 
 export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres: Genre[];
+  publishers: Publisher[];
   description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
