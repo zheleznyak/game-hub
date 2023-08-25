feeff2a30965d567a57141e9562c1ba1c895f923
zheleznyak <mobobzor@gmail.com>
2023-08-25 16:15:10 +0200

    Fix Layout and Navbar

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useParams} from "react-router-dom";
 import useGame from "../hooks/useGame";
-import {Heading, Spinner} from "@chakra-ui/react";
+import {Heading, Spinner, SimpleGrid, GridItem} from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import GamesTrailer from "../components/GamesTrailer";
@@ -15,13 +15,17 @@ const GameDetailPage = () => {
   if (error || !game) throw error;
 
   return (
-    <>
-      <Heading>{ game.name }</Heading>
-      <ExpandableText>{ game.description_raw }</ExpandableText>
-      <GameAttributes game={game} />
-      <GamesTrailer gameId={game.id} />
-      <GameScreenshots gameId={game.id} />
-    </>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+      <GridItem>
+        <Heading>{ game.name }</Heading>
+        <ExpandableText>{ game.description_raw }</ExpandableText>
+        <GameAttributes game={game} />
+      </GridItem>
+      <GridItem>
+        <GamesTrailer gameId={game.id} />
+        <GameScreenshots gameId={game.id} />
+      </GridItem>
+    </SimpleGrid>
   );
 };
 
